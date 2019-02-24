e284b0fc5de9fe4dc10b945f040b9fe71cfd5fa7
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-02-23 22:55:15 -0700

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  user: 'root',
-  password: 'orange102938',
-  port: 3306,
-  database: 'burgers_db',
-  host: 'localhost'
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    user: 'root',
+    password: 'orange102938',
+    port: 3306,
+    database: 'burgers_db',
+    host: 'localhost'
+  });
+}
+
+connection.connect();
 
 module.exports = connection;
